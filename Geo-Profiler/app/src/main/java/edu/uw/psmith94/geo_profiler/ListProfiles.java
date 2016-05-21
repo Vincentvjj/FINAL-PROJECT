e2ba81f19cd2d819b16d4f0a869d36af26c2db45
vincent <jonanv@uw.edu>
2016-05-21 12:02:39 -0700

    added activities and laypouts

---
+++ Geo-Profiler/app/src/main/java/edu/uw/psmith94/geo_profiler/ListProfiles.java
@@ -0,0 +1,16 @@
+package edu.uw.psmith94.geo_profiler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vincentjoe on 5/21/16.
+ */
+public class ListProfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.list_profiles);
+        super.onCreate(savedInstanceState);
+    }
+}
