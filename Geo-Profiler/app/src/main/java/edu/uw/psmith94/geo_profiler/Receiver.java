9845b48d3ffa3ef8eb97f86b258004ab63c5b870
vincent <jonanv@uw.edu>
2016-05-29 16:37:22 -0700

    Add receiver

--- Geo-Profiler/app/src/main/java/edu/uw/psmith94/geo_profiler/Receiver.java
+++ Geo-Profiler/app/src/main/java/edu/uw/psmith94/geo_profiler/Receiver.java
@@ -1,20 +1,37 @@
 package edu.uw.psmith94.geo_profiler;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.provider.Telephony;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import edu.uw.profile.provider.Profile;
+import edu.uw.profile.provider.ProfileProvider;
 
 
 /**
@@ -23,88 +40,73 @@ import android.util.Log;
 
 public class Receiver extends BroadcastReceiver {
 
-    public static final String TAG = "ReceiveMessage";
-    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
-    public static final String ACTION_SMS_SENT = "edu.uw.notsetdemo.ACTION_SMS_SENT";
-    public static final int SMS_NOTIFY_CODE = 3;
-
-    private static final int SMS_SEND_CODE = 1;
-
-    private String num;
-    private String body;
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        final Bundle bundle = intent.getExtras();
-        if(intent.getAction().equals(SMS_RECEIVED)) {
-            Log.v(TAG, "received");
-            {
-                SmsMessage message = Telephony.Sms.Intents.getMessagesFromIntent(intent)[0];
+        if (intent.getAction() == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
 
+            SmsMessage[] sms = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+            String author = sms[0].getOriginatingAddress();
+
+            Calendar c = Calendar.getInstance();
+            int today = c.get(Calendar.DAY_OF_WEEK);
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int min = c.get(Calendar.MINUTE);
+
+            String dayContent = "";
+
+            switch (today) {
+                case 1:
+                    dayContent = Profile.SUN;
+                case 2:
+                    dayContent = Profile.MON;
+                case 3:
+                    dayContent = Profile.TUES;
+                case 4:
+                    dayContent = Profile.WED;
+                case 5:
+                    dayContent = Profile.THUR;
+                case 6:
+                    dayContent = Profile.FRI;
+                case 7:
+                    dayContent = Profile.SAT;
+                default:
+                    dayContent = Profile.MON;
             }
-            try {
-                Object[] pdus = (Object[]) bundle.get("pdus");
 
-                for (int i = 0; i < pdus.length; i++) {
-                    SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                    num = message.getDisplayOriginatingAddress();
-                    body = message.getMessageBody();
+            String[] projection = new String[]{Profile.ID, Profile.LAT, Profile.LNG, Profile.SHAPE, Profile.MESSAGE,
+                    Profile.RADIUS, Profile.TIME_END, Profile.TIME_START, dayContent, Profile.ACTIVE};
 
-                    notify(context, message);
-                }
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-                if (preferences.getBoolean("pref_auto", false)) {
-                    SmsManager smsManager = SmsManager.getDefault();
-                    String response = preferences.getString("pref_edit_text", "Response").toString();
-                    if (response == "") {
-                        response = "<3";
-                    }
-                    Intent smsIntent = new Intent(ACTION_SMS_SENT);
-                    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, SMS_SEND_CODE, smsIntent, 0);
-                    smsManager.sendTextMessage(
-                            num,
-                            null,
-                            response,
-                            pendingIntent,
-                            null
-                    );
-                }
+//            String whereClause = Profile.ACTIVE + "= 1" + " AND " + dayContent + " = 1";
 
+            Cursor cur = context.getContentResolver().query(ProfileProvider.CONTENT_URI, projection,
+                    null, null, null);
 
-            } catch (Exception e) {
+            //get my last coordinate
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
+            int permissionCheck = ContextCompat.checkSelfPermission
+                    (context, Manifest.permission.ACCESS_FINE_LOCATION); // getpermision
+            if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                Location lo = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Log.v("TAG", lo.toString());
+//                locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
             }
+            else {
+                ActivityCompat.requestPermissions((Activity)context, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+
+
+            while(cur.moveToNext()){
+                //checks time first, then the coordinates
+                Log.v("TAG", (cur.getString(cur.getColumnIndex(Profile.MESSAGE))));
+            }
+            cur.close();
+
         }
     }
 
-    private static final int NOTIFY_CODE = 0;
 
-
-    public void notify(Context context, SmsMessage message){
-        Log.v(TAG, "Notify button pressed");
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentTitle(num)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setContentText(body);
-
-        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
-        builder.setVibrate(new long[]{0, 500, 500, 500});
-        builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
-
-
-        Intent intent = new Intent(context, MapsActivity.class);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addNextIntent(intent);
-        PendingIntent pendingIntent = stackBuilder.getPendingIntent(SMS_NOTIFY_CODE, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.setContentIntent(pendingIntent); //what to happen when clicked
-
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(NOTIFY_CODE, builder.build());
-    }
 }
 
 
